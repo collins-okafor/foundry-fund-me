70d1993f5c4bd347a80bdf00a332feba50d97316
Collins Okafor <collinszurum@gmail.com>
2023-10-05 11:46:37 +0100

    chore: forge init

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+
+contract FundMeTest is Test {
+    function setUp() external {}
+
+    function testDemo() public {}
+ 
+}
