835acc6409b63f974e02a339943e0f5b9e6b5028
Collins Okafor <69325061+collins-okafor@users.noreply.github.com>
2023-10-19 19:12:52 +0100

    comment removal

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -6,14 +6,14 @@ import {AggregatorV3Interface} from "lib/chainlink-brownie-contracts/contracts/s
 
 
 library PriceConverter {
-    // We could make this public, but then we'd have to deploy it
+    
     function getPrice() internal view returns (uint256) {
        
         AggregatorV3Interface priceFeed = AggregatorV3Interface(
             0x694AA1769357215DE4FAC081bf1f309aDC325306
         );
         (, int256 answer, , , ) = priceFeed.latestRoundData();
-        // ETH/USD rate in 18 digit
+        
         return uint256(answer * 10000000000);
     }
 
